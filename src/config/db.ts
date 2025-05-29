89d577d3fcf6f0d013de6276433d348c69582aa8
Shubham <shubhamdhingra9896@gmail.com>
2025-05-29 17:54:43 +0530

    added some changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
-const connectionString = process.env.DATABASE_URL || "";
+export const connectionString = process.env.DATABASE_URL || "";
 export const db = mysql.createPool(connectionString);
