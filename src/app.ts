3cc95530f61f62d26d4da744585c824bb1615654
Shubham <shubhamdhingra9896@gmail.com>
2025-05-30 12:58:34 +0530

    added soem changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import identifyRouter from "./routes/identify";
-import mysql from "mysql2/promise";
-import { connectionString } from "./config/db";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use("/identify", identifyRouter);
-
+app.get("/", (req, res) => {
+  res.send("API is running on /identify");
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});export const db = mysql.createPool(connectionString);
-
+});
