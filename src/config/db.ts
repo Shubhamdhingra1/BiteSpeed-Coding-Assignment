49df0f5a81e6f7a80651ff4f98660a026d32f2dc
Shubham <shubhamdhingra9896@gmail.com>
2025-05-30 12:24:00 +0530

    added some changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,11 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
-export const connectionString = process.env.DATABASE_URL;
-export const db = mysql.createPool(connectionString);
+export const getConnectionString = () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+  }
+  return process.env.DATABASE_URL;
+};
+
+export const db = mysql.createPool(getConnectionString());
